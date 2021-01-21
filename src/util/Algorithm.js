8d7c831815e742668328b390a4fa8baf79244783
Tyler O'Brien <hello@tylerobrien.com>
2021-01-21 14:54:12 -0500

    Remove assertions

--- src/util/Algorithm.js
+++ src/util/Algorithm.js
@@ -21,8 +21,6 @@ import { JS } from './JS';
  * @return {array}
  */
 export function truthies(items) {
-  JS.AssertType('items', 'array', typeof items, JS.isArray);
-
   const result = [];
   const end = items.length;
 
@@ -50,8 +48,6 @@ export function truthies(items) {
  * @return {array}
  */
 export function search(items, needle, operator) {
-  JS.AssertType('items', 'array', typeof items, JS.isArray);
-
   const result = [];
   const end = items.length;
 
@@ -78,11 +74,6 @@ export function search(items, needle, operator) {
  * @return {*}
  */
 export function within(source, key) {
-  JS.AssertTypes([
-    [ 'source', 'object', typeof source,  JS.isObject ],
-    [ 'key',    'string', typeof key,     JS.isString ]
-  ]);
-  
   if (!key) {
     ThrowUnexpectedEmpty('key', 'string');
   }
@@ -108,10 +99,6 @@ export function within(source, key) {
  * @return {object}
  */
 export function expand(condensed) {
-  if (!JS.isObject(condensed)) {
-    ThrowUnexpectedTypeError('condensed', 'object', typeof condensed);
-  }
-
   const expanded = {};
 
   for (const key in condensed) {
@@ -148,12 +135,6 @@ export function expand(condensed) {
  * @return {object}
  */
 export function condense(source, parent, existing) {
-  JS.AssertTypes([
-    [ 'source',   'object',           typeof source,    JS.isObject ],
-    [ 'parent',   'array|undefined',  typeof parent,    JS.isUndefined, JS.isString ],
-    [ 'existing', 'object|undefined', typeof existing,  JS.isUndefined, JS.isObject ]
-  ]);
-  
   if (!parent) {
     parent = [];
   }
@@ -180,14 +161,6 @@ export function condense(source, parent, existing) {
  * @return {object}
  */
 export function excepted(source, except) {
-  if (!JS.isObject(source)) {
-    ThrowUnexpectedTypeError('source', 'object', typeof source);
-  }
-
-  if (!JS.isObject(except) && !JS.isArray(except)) {
-    ThrowUnexpectedTypeError('except', 'array|object', typeof except);
-  }
-
   const clone = Object.assign({}, source);
 
   if (JS.isArray(except)) {
