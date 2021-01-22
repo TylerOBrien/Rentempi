01fc0782b80c4da834675c9455e08249b2b40bbc
Tyler O'Brien <hello@tylerobrien.com>
2021-01-21 22:26:12 -0500

    Add TypeScript support

---
+++ src/hooks/Storage.ts
@@ -0,0 +1,95 @@
+/**
+ * Local Imports
+*/
+
+import { StorageDriver } from '~/system/Storage';
+import { Storage } from '~/util';
+
+/**
+ * Types/Interfaces
+*/
+
+export interface StorageHook {
+  keys():Promise<string[]>;
+  has(key:string):Promise<boolean>;
+  get(key:string):Promise<any>;
+  set(key:string, value:any):Promise<void>;
+  merge(key:string, value:any):Promise<void>;
+  remove(key:string):Promise<void>;
+  clear():Promise<void>;
+}
+
+/**
+ * Exports
+*/
+
+export function useStorage(group:string='db'):StorageHook {
+  /** Helpers **/
+
+  /**
+   * 
+   */
+  const has = (key:string):Promise<boolean> => {
+    return new Promise((resolve, reject) => {
+      StorageDriver.keys()
+        .then(async keys => resolve(keys.indexOf(await Storage.name(group, key)) !== -1))
+        .catch(reject);
+    });
+  };
+
+  /**
+   * 
+   */
+  const get = (key:string):Promise<any> => {
+    return new Promise((resolve, reject) => {
+      StorageDriver.get(group, key)
+        .then(data => resolve(data ? JSON.parse(data) : undefined))
+        .catch(reject);
+    });
+  };
+  
+  /**
+   * 
+   */
+  const set = (key:string, value:any):Promise<void> => {
+    return new Promise((resolve, reject) => {
+      StorageDriver.set(group, key, value)
+        .then(resolve)
+        .catch(reject);
+    });
+  };
+  
+  /**
+   * 
+   */
+  const merge = (key:string, value:any):Promise<void> => {
+    return new Promise((resolve, reject) => {
+      StorageDriver.merge(group, key, value)
+        .then(resolve)
+        .catch(reject);
+    });
+  };
+  
+  /**
+   * 
+   */
+  const remove = (key:string):Promise<void> => {
+    return new Promise((resolve, reject) => {
+      StorageDriver.remove(group, key)
+        .then(resolve)
+        .catch(reject);
+    });
+  };
+  
+  /** Output **/
+  
+  return {
+    has,
+    get,
+    set,
+    merge,
+    remove,
+    keys: StorageDriver.keys,
+    clear: StorageDriver.clear
+  };
+}
