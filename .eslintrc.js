5cdd99c280dccaad90e9107562147cdd69d51f09
Tyler O'Brien <hello@tylerobrien.com>
2021-01-21 20:54:34 -0500

    Update eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,18 @@
 module.exports = {
   root: true,
+  parser: 'babel-eslint',
   env: {
     es6: true
   },
   parserOptions: {
     sourceType: 'module'
-  }
+  },
+  plugins: [
+    'eslint-comments',
+    'react',
+    'react-hooks',
+    'react-native',
+    '@react-native-community',
+    'jest'
+  ],
 };
