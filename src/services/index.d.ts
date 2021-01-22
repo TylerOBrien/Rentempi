1452a08fd4110f8c847d05606d4d375a0d14fde6
Tyler O'Brien <hello@tylerobrien.com>
2021-01-22 00:32:34 -0500

    Add services index

---
+++ src/services/index.d.ts
@@ -0,0 +1,23 @@
+/**
+ * Sibling Imports
+*/
+
+import * as AddressServices from './Address';
+import * as AuthServices from './Auth';
+import * as EmailVerificationServices from './EmailVerification';
+import * as ImageServices from './Image';
+import * as ProfileServices from './Profile';
+import * as UserServices from './User';
+
+/**
+ * Exports
+*/
+
+export {
+  AddressServices,
+  AuthServices,
+  EmailVerificationServices,
+  ImageServices,
+  ProfileServices,
+  UserServices
+}
