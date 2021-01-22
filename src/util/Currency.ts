6499d7b9f4e58236667b9077b03442539ab87e3e
Tyler O'Brien <hello@tylerobrien.com>
2021-01-21 21:23:42 -0500

    Restructure and clean up code

--- src/util/Currency.ts
+++ src/util/Currency.ts
@@ -11,47 +11,14 @@ const twoDecimalsRegex = new RegExp('\.\d{2}$');
 */
 
 /**
- * 
- * 
- * @param {number} cents
- * @param {string} currencySign
- * @param {string} thousandsSeparator
- * @param {string} decimalPoint
- * 
- * @return {string}
- */
-export function currencyString(cents:number, currencySign:string='$', thousandsSeparator:string=',', decimalPoint:string='.'):string {
-  const currency = (cents / 100).toFixed(2);
-
-  if (decimalPoint !== '.') {
-    currency[currency.length-2] = decimalPoint;
-  }
-
-  if (cents < 100000) {
-    return currencySign + currency;
-  }
-
-  let formatted = currency.slice(-3);
-  const end = currency.length;
-
-  for (let i = (end-3); i > 2; i -= 3) {
-    formatted = ( (i === 3 ? '' : thousandsSeparator) + currency.slice(i-3, i) ) + formatted;
-  }
-
-  return currencySign + currency.slice(0, end % 3) + formatted;
-}
-
-/**
- * 
- * 
- * @param {string} currencyString
+ * @param {string} localeString
  * 
  * @return {number}
  */
-export function cents(currencyString:string, thousandsSeparator:string=',', decimalPoint:string='.'):number {
+export function cents(localeString:string, thousandsSeparator:string=',', decimalPoint:string='.'):number {
   const thousandsSeparatorRegex = ( thousandsSeparator === ',' ? commaRegex : new RegExp(thousandsSeparator, 'g') );
 
-  let value = currencyString.replace(thousandsSeparatorRegex, '');
+  let value = localeString.replace(thousandsSeparatorRegex, '');
 
   if (value.match(twoDecimalsRegex)) {
     value = value.replace(decimalPoint, '');
@@ -63,3 +30,47 @@ export function cents(currencyString:string, thousandsSeparator:string=',', deci
 
   return parseInt(value.slice(1));
 }
+
+/**
+ * @param {number} cents
+ * @param {string} currencySign
+ * @param {string} thousandsSeparator
+ * @param {string} decimalPoint
+ * 
+ * @return {string}
+ */
+export function localeString(cents:number, currencySign:string='$', thousandsSeparator:string=',', decimalPoint:string='.'):string {
+  let currency = (cents / 100).toFixed(2);
+
+  if (decimalPoint !== '.') {
+    const pivot = currency.indexOf('.');
+    currency = currency.substring(0, pivot) + decimalPoint + currency.substring(pivot + 1);
+  }
+
+  // If less than $1000.00 then there are no thousands separators to be added.
+
+  if (cents < 100000) {
+    return currencySign + currency;
+  }
+
+  // Add thousands separators.
+
+  let formatted = currency.slice(-3);
+  const end = currency.length;
+
+  // Iterate backward, skipping the last three characters (i.e. the decimal
+  // point and two digits representing the cents), adding a separator every
+  // three characters.
+
+  for (let i = (end-3); i > 2; i -= 3) {
+    formatted = ( (i === 3 ? '' : thousandsSeparator) + currency.slice(i-3, i) ) + formatted;
+  }
+
+  return currencySign + currency.slice(0, end % 3) + formatted;
+}
+
+/**
+ * Namespaced Exports
+*/
+
+export const Currency = { cents, localeString };
