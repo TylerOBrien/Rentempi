f27832fb8b3a058a1c2ad1bf3021824b38f11522
Tyler O'Brien <hello@tylerobrien.com>
2021-01-25 18:14:14 -0500

    Update images module

--- resources/images/index.js
+++ resources/images/index.js
@@ -8,32 +8,24 @@ import { Image } from 'react-native';
  * Local Imports
 */
 
-import StepsInsituLogoPng from './StepsInsituLogo.png';
-import SwooshPng from './Swoosh.png';
+import LogoPng from './Logo.png';
 
 /**
  * Locals
 */
 
-/**
- * 
- */
 function imageInfo(image) {
   const info = Image.resolveAssetSource(image);
   info.aspectRatio = info.width / info.height;
   return info;
 }
 
-/**
- * 
- */
 function imageExport(source) {
-  return { source, ...imageInfo(source) };
+  return Object.assign({}, source, imageInfo(source));
 }
 
 /**
  * Exports
 */
 
-export const Logo = imageExport(StepsInsituLogoPng);
-export const Swoosh = imageExport(SwooshPng);
+export const Logo = imageExport(LogoPng);
