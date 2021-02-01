2cdd7b51eccfb8346dd00da2b6dafdc51784b346
Tyler O'Brien <hello@tylerobrien.com>
2021-01-31 23:17:21 -0500

    Add TypeScript support

---
+++ src/providers/WebSocketProvider.tsx
@@ -0,0 +1,39 @@
+/**
+ * Global Imports
+*/
+
+import React, { useRef, useState } from 'react';
+
+/**
+ * Types/Interfaces
+*/
+
+export interface WebSocketContextInterface {
+  isConnected: boolean;
+  setIsConnected: React.Dispatch<React.SetStateAction<boolean>>;
+  wsRef: React.MutableRefObject<object>;
+};
+
+/**
+ * Exports
+*/
+
+export const WebSocketContext = React.createContext<WebSocketContextInterface>(undefined);
+
+export function WebSocketProvider(props:any) {
+  /** Refs **/
+
+  const wsRef = useRef<object>();
+
+  /** States **/
+
+  const [ isConnected, setIsConnected ] = useState<boolean>();
+  
+  /** Output **/
+  
+  return (
+    <WebSocketContext.Provider value={{ isConnected, setIsConnected, wsRef }}>
+      { props.children }
+    </WebSocketContext.Provider>
+  );
+}
