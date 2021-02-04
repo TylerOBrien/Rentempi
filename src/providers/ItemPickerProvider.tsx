57a9a71a54a0376e12e84377e4ef34b63878e487
Tyler O'Brien <hello@tylerobrien.com>
2021-02-04 16:23:20 -0500

    Add TypeScript support

--- src/providers/ItemPickerProvider.tsx
+++ src/providers/ItemPickerProvider.tsx
@@ -8,48 +8,70 @@ import React, { useContext, useEffect, useRef, useState } from 'react';
  * Local Imports
 */
 
+import { Pressable, Text, View } from '~/components/Base';
 import { Modal } from '~/components/Modal';
-import { Pressable } from '~/components/Pressable';
-import { Text, View } from '~/components/Base';
 
 /**
- * Exports
+ * Types/Interfaces
 */
 
-export const ItemPickerContext = React.createContext();
+export interface ItemPickerContextInterface {
+  isActive: boolean;
+  setIsActive: React.Dispatch<React.SetStateAction<boolean>>;
+
+  itemsRef: React.MutableRefObject<Array<any>>;
+  selectedValueRef: React.MutableRefObject<any>;
+  handleChooseRef: React.MutableRefObject<Function>;
+  handleCancelRef: React.MutableRefObject<Function>;
+  handleResetRef: React.MutableRefObject<Function>;
+};
 
 /**
- * 
- */
-export function ItemPickerProvider(props) {
+ * Contexts
+*/
+
+export const ItemPickerContext = React.createContext<ItemPickerContextInterface>(undefined);
+
+/**
+ * Components
+*/
+
+export function ItemPickerProvider(props:any) {
   /** Refs **/
 
-  const itemsRef = useRef([]);
-  const selectedValueRef = useRef();
-  const handleChooseRef = useRef();
-  const handleCancelRef = useRef();
-  const handleResetRef = useRef();
+  const itemsRef = useRef<Array<any>>();
+  const selectedValueRef = useRef<any>();
+  const handleChooseRef = useRef<Function>();
+  const handleCancelRef = useRef<Function>();
+  const handleResetRef = useRef<Function>();
 
   /** States **/
 
-  const [ isActive, setIsActive ] = useState(false);
+  const [ isActive, setIsActive ] = useState<boolean>();
 
   /** Output **/
 
   return (
-    <ItemPickerContext.Provider value={{ itemsRef, selectedValueRef, handleChooseRef, handleCancelRef, handleResetRef, isActive, setIsActive }}>
+    <ItemPickerContext.Provider
+      value={{
+        isActive,
+        setIsActive,
+        itemsRef,
+        selectedValueRef,
+        handleChooseRef,
+        handleCancelRef,
+        handleResetRef
+      }}
+    >
       { props.children }
     </ItemPickerContext.Provider>
   );
 }
 
-/**
- * 
- */
-export function ItemPickerBase(props) {
+export function ItemPickerBase() {
   /** Contexts **/
   
-  const { itemsRef, selectedValueRef, handleChooseRef, handleCancelRef, handleResetRef, isActive, setIsActive } = useContext(ItemPickerContext);
+  const { isActive, setIsActive, itemsRef, selectedValueRef, handleChooseRef, handleCancelRef, handleResetRef } = useContext(ItemPickerContext);
   
   /** Event Handlers **/
 
