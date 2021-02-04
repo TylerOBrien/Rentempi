6c65082fda5f97ee03daf6ce0b48812405c14e22
Tyler O'Brien <hello@tylerobrien.com>
2021-02-04 17:55:30 -0500

    Restructure code to better use TypeScript

--- src/config/Storage.ts
+++ src/config/Storage.ts
@@ -11,23 +11,47 @@ import Config from 'react-native-config';
 import { AppConfig } from './App';
 
 /**
+ * Types/Interfaces
+*/
+
+export type StorageSlug = string;
+
+export interface StorageGroups {
+  default: string;
+};
+
+export interface StorageKeys {
+  token: string;
+};
+
+export interface StorageNames {
+  prefix: string;
+  suffix: string;
+  delimeter: string;
+};
+
+/**
+ * Locals
+*/
+
+const slug:StorageSlug = Config.STORAGE_SLUG || AppConfig.slug;
+
+const group:StorageGroups = {
+  default: Config.STORAGE_DEFAULT_GROUP || 'storage'
+};
+
+const key:StorageKeys = {
+  token: 'token'
+};
+
+const name:StorageNames = {
+  prefix: Config.STORAGE_NAME_PREFIX || '@',
+  suffix: Config.STORAGE_NAME_SUFFIX || '',
+  delimeter: Config.STORAGE_NAME_DELIMETER || ':'
+};
+
+/**
  * Exports
 */
 
-export const StorageConfig = {
-  slug: Config.STORAGE_SLUG || AppConfig.slug,
-
-  group: {
-    default: Config.STORAGE_DEFAULT_GROUP || 'storage'
-  },
-
-  key: {
-    token: 'token'
-  },
-
-  name: {
-    prefix: Config.STORAGE_NAME_PREFIX || '@',
-    suffix: Config.STORAGE_NAME_SUFFIX || '',
-    delimeter: Config.STORAGE_NAME_DELIMETER || ':'
-  }
-};
+export const StorageConfig = { slug, group, key, name };
