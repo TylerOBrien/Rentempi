f0ec2a5a833a40714c35ecfb900974a2030189ee
Tyler O'Brien <hello@tylerobrien.com>
2021-02-04 17:44:13 -0500

    Restructure code to better use TypeScript

--- src/config/Theme.ts
+++ src/config/Theme.ts
@@ -2,7 +2,7 @@
  * Global Imports
 */
 
-import { StyleSheet } from 'react-native';
+import { StatusBarStyle, StyleSheet } from 'react-native';
 
 /**
  * Local Imports
@@ -11,56 +11,71 @@ import { StyleSheet } from 'react-native';
 import { Tailwind } from '~/util/TailwindCss';
 
 /**
- * Exports
+ * Types/Interfaces
 */
 
-export const ThemeConfig = {
-  tailwind: {
-    button: {
-      container: 'flex-initial justify-center rounded-full',
-      label: 'py-3 px-12 text-center text-white text-base'
-    },
-    input: {
-      target: 'text-base pl-2',
-      label: 'pl-2'
-    },
-    text: {
-
-    },
-    modal: {
-      
-    }
-  },
-
-  style: StyleSheet.create({
-    button: {
-
-    },
-    text: {
-
-    },
-    modal: {
-
-    }
-  }),
-
-  color: {
-    primary: Tailwind.color('primary'),
-    secondary: Tailwind.color('secondary'),
-    tertiary: Tailwind.color('tertiary'),
-
-    success: Tailwind.color('success'),
-    fail: Tailwind.color('fail'),
-    warning: Tailwind.color('warning'),
-    error: Tailwind.color('error'),
-    info: Tailwind.color('info'),
-    notice: Tailwind.color('notice')
-  },
-
-  defaults: {
-    statusBarStyle: 'light-content',
-    group: {
-      name: 'container'
-    }
-  },
+export interface ThemeDefaults {
+  statusBarStyle: StatusBarStyle;
+  group: {
+    name: string;
+  };
 };
+
+/**
+ * Locals
+*/
+
+const tailwind = {
+  button: {
+    container: 'flex-initial justify-center rounded-full',
+    label: 'py-3 px-12 text-center text-white text-base'
+  },
+  input: {
+    target: 'text-base pl-2',
+    label: 'pl-2'
+  },
+  text: {
+
+  },
+  modal: {
+    
+  }
+};
+
+const style = StyleSheet.create({
+  button: {
+
+  },
+  text: {
+
+  },
+  modal: {
+
+  }
+});
+
+const color = {
+  primary: Tailwind.color('primary'),
+  secondary: Tailwind.color('secondary'),
+  tertiary: Tailwind.color('tertiary'),
+
+  success: Tailwind.color('success'),
+  fail: Tailwind.color('fail'),
+  warning: Tailwind.color('warning'),
+  error: Tailwind.color('error'),
+  info: Tailwind.color('info'),
+  notice: Tailwind.color('notice')
+};
+
+const defaults:ThemeDefaults = {
+  statusBarStyle: 'light-content',
+  group: {
+    name: 'container'
+  }
+};
+
+/**
+ * Namespaced Exports
+*/
+
+export const ThemeConfig = { tailwind, style, color, defaults };
