563573bd3741ac418fca843bdfdfae0bb8580f30
Tyler O'Brien <hello@tylerobrien.com>
2021-02-05 23:14:11 -0500

    Fix type defs

--- src/components/Grid/Row.tsx
+++ src/components/Grid/Row.tsx
@@ -21,7 +21,9 @@ export type Cols = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13 | 14 | 
 
 export interface RowProps extends TailwindEnabledProps {
   cols?: Cols;
+  children?: ReactNode;
 };
+
 /**
  * Exports
 */
