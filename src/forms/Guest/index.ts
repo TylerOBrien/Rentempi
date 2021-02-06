42e2a78eeb9a073255b2d92a6c96dcc4424cb8b5
Tyler O'Brien <hello@tylerobrien.com>
2021-02-06 18:36:05 -0500

    Export forms

--- src/forms/Guest/index.ts
+++ src/forms/Guest/index.ts
@@ -2,5 +2,7 @@
  * Exports
 */
 
+export { ForgotPasswordForm } from './ForgotPasswordForm';
 export { LoginForm } from './LoginForm';
 export { RegisterForm } from './RegisterForm';
+export { ResetPasswordForm } from './ResetPasswordForm';
