44baa9fc51e2df8c67c81e90f779d150d1ebd445
Tyler O'Brien <hello@tylerobrien.com>
2021-02-06 12:28:33 -0500

    Fix login

--- src/screens/Guest/Login/Login.tsx
+++ src/screens/Guest/Login/Login.tsx
@@ -15,6 +15,8 @@ import { PrimaryGuestLayout } from '~/layouts/Guest';
 import { useAuth, useForm, useService } from '~/hooks';
 import { FormHook } from '~/hooks/Form';
 
+import { LoginAuthResponse } from '~/services/Auth';
+
 /**
  * Types/Interfaces
 */
@@ -52,12 +54,17 @@ export function Login(props:LoginProps) {
   
   /** Event Handlers **/
   
-  const handleSuccess = ({ user, token }) => {
-    auth.login({ user, token }, { remember });
+  const handleSuccess = (response:LoginAuthResponse) => {
+    auth.login({
+      user: response.user,
+      credentials: { token: response.token.value }
+    }, {
+      remember
+    });
   };
   
   const handleSubmit = (values:LoginFields, formik:FormikHelpers<LoginFields>) => {
-    service.call('Auth.Login', values)
+    service.call<LoginAuthResponse>('Auth.Login', values)
       .then(handleSuccess)
       .catch(error => {
         form.setError(error);
