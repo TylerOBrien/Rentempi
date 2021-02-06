e3445cd1a72ebe7ae33c12049c6f57b0d46c1550
Tyler O'Brien <hello@tylerobrien.com>
2021-02-06 12:52:43 -0500

    Add Functional util

---
+++ src/util/Functional.ts
@@ -0,0 +1,23 @@
+/**
+ * Locals
+*/
+
+/**
+ * Delays execution of callback. Primarily only useful to prevent blocking of
+ * React Native's UI thread.
+ * 
+ * @param {Function} callback
+ * 
+ * @return {Function}
+ */
+function delayed(callback:Function, ...args:Array<any>):Function {
+  return () => {
+    requestAnimationFrame(() => callback(...args));
+  };
+}
+
+/**
+ * Namespaced Exports
+*/
+
+export const Functional = { delayed };
