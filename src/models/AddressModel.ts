cfa602ea88f2f2758a0be19b4eb03c08679a6de3
Tyler O'Brien <hello@tylerobrien.com>
2021-02-07 14:46:42 -0500

    Make fields optional

--- src/models/AddressModel.ts
+++ src/models/AddressModel.ts
@@ -4,10 +4,10 @@
 
 export interface AddressModel {
   id: number;
-  line1: string;
+  line1?: string;
   line2?: string;
   city: string;
   province: string;
   country: string;
-  postal_code: string;
+  postal_code?: string;
 };
