987e0aedce71085577c3a2ba5a816e26bd7a5c81
Tyler O'Brien <hello@tylerobrien.com>
2021-02-06 22:03:53 -0500

    Add basic alert functionality

--- src/components/Alert/Alert.tsx
+++ src/components/Alert/Alert.tsx
@@ -3,12 +3,14 @@
 */
 
 import React from 'react';
+import { StyleSheet } from 'react-native';
+import FaIcon from 'react-native-vector-icons/FontAwesome';
 
 /**
  * Local Imports
 */
 
-import { Text, View } from '~/components/Base';
+import { Pressable, Text, View } from '~/components/Base';
 import { AlertItem } from '~/hooks/Alerter';
 import { TailwindEnabledProps } from '~/util/TailwindCss'
 
@@ -16,9 +18,8 @@ import { TailwindEnabledProps } from '~/util/TailwindCss'
  * Types/Interfaces
 */
 
-export interface AlertProps extends TailwindEnabledProps {
-  type: string;
-  value: string;
+export interface AlertProps extends AlertItem, TailwindEnabledProps {
+  onClose: Function;
 }
 
 /**
@@ -27,8 +28,38 @@ export interface AlertProps extends TailwindEnabledProps {
 
 export function Alert(props:AlertProps) {
   return (
-    <View style={{ backgroundColor: 'red' }}>
-      <Text>{ props.value }</Text>
+    <View style={ styles.container }>
+      <Text style={ styles.text }>{ props.value }</Text>
+      <Pressable onPress={ props.onClose }>
+        <FaIcon
+          name='close'
+          size={ 18 }
+          style={ styles.icon }
+        />
+      </Pressable>
     </View>
   );
 }
+
+/**
+ * Styles
+*/
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    alignSelf: 'center',
+    padding: 16,
+    borderRadius: 128,
+    backgroundColor: 'red'
+  },
+
+  text: {
+    color: 'white'
+  },
+
+  icon: {
+    color: 'white',
+    marginLeft: 8
+  }
+});
