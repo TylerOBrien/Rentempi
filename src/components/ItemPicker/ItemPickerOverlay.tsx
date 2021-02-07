7ec5dea9120fc48d6c01cc14f7e9cc2257aa4988
Tyler O'Brien <hello@tylerobrien.com>
2021-02-07 13:29:29 -0500

    Render ItemPicker items

--- src/components/ItemPicker/ItemPickerOverlay.tsx
+++ src/components/ItemPicker/ItemPickerOverlay.tsx
@@ -9,9 +9,17 @@ import { StyleSheet } from 'react-native';
  * Local Imports
 */
 
-import { Text, View } from '~/components/Base';
+import { Pressable, Text, ScrollView, View } from '~/components/Base';
 import { Modal } from '~/components/Modal';
-import { ItemPickerContext } from '~/providers/ItemPickerProvider';
+import { ItemPickerContext, ItemPickerItem } from '~/providers/ItemPickerProvider';
+
+/**
+ * Locals
+*/
+
+const itemAndroidRipple = {
+  radius: 256
+};
 
 /**
  * Components
@@ -20,14 +28,33 @@ import { ItemPickerContext } from '~/providers/ItemPickerProvider';
 export function ItemPickerOverlay() {
   /** Contexts **/
 
-  const { isActive } = useContext(ItemPickerContext);
+  const { isActive, setIsActive, itemsRef } = useContext(ItemPickerContext);
+
+  /** Event Handlers **/
+
+  const handlePress = (item:ItemPickerItem, index:number) => {
+    //
+  };
 
   /** Output **/
   
   return (
     <Modal visible={ isActive }>
       <View style={ styles.container }>
-        
+        <ScrollView style={ styles.scroller }>
+          {
+            (itemsRef.current || []).map((item, index) => (
+              <Pressable
+                key={ index }
+                style={ styles.item }
+                android_ripple={ itemAndroidRipple }
+                onPress={ handlePress.bind(this, item, index) }
+              >
+                <Text>{ item.name }</Text>
+              </Pressable>
+            ))
+          }
+        </ScrollView>
       </View>
     </Modal>
   );
@@ -38,7 +65,32 @@ export function ItemPickerOverlay() {
 */
 
 const styles = StyleSheet.create({
+  modal: {
+    alignSelf: 'center',
+    backgroundColor: 'blue'
+  },
+
   container: {
-    
+    width: 300
+  },
+
+  scroller: {
+    height: 400,
+    borderRadius: 16
+  },
+
+  item: {
+    padding: 16,
+    backgroundColor: 'white'
+  },
+
+  itemFirst: {
+    borderTopLeftRadius: 16,
+    borderTopRightRadius: 16
+  },
+
+  itemLast: {
+    borderBottomLeftRadius: 16,
+    borderBottomRightRadius: 16
   }
 });
