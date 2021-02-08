b3f4f2e76ba45d7173c6ea51c57d52720c0fb43b
Tyler O'Brien <hello@tylerobrien.com>
2021-02-07 21:50:44 -0500

    Update styling

--- src/screens/Guest/Welcome/components/Greeting.tsx
+++ src/screens/Guest/Welcome/components/Greeting.tsx
@@ -25,7 +25,7 @@ export interface GreetingProps {
 export function Greeting(props:GreetingProps) {
   return (
     <View tailwind='py-8'>
-      <Text tailwind='text-base text-center text-black'>
+      <Text tailwind='text-lg text-center text-black'>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris nec ligula malesuada, condimentum lorem sit
         amet, dictum metus. Ut tempor ante dui, eget accumsan arcu placerat euismod.
       </Text>
