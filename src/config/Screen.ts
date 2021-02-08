6268ad494a73497ec84e7ff6a15300a60f4eece0
Tyler O'Brien <hello@tylerobrien.com>
2021-02-07 19:09:25 -0500

    Update ScreenConfig

--- src/config/Screen.ts
+++ src/config/Screen.ts
@@ -1,11 +1,21 @@
 /**
+ * Global Imports
+*/
+
+import { Dimensions } from 'react-native';
+
+/**
  * Exports
 */
 
 export const ScreenConfig = {
+  dimensions: {
+    min: Math.min(Dimensions.get('window').width, Dimensions.get('window').height),
+    max: Math.max(Dimensions.get('window').width, Dimensions.get('window').height)
+  },
   initial: {
     Guest: 'Welcome',
-    Unverified: 'VerifyEmail',
+    Unverified: 'Verify Email',
     Verified: 'Lobby'
   }
 };
