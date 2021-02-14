f5406272f2c664c6bf8ac80e5051e38b445860eb
Tyler O'Brien <hello@tylerobrien.com>
2021-02-14 17:53:53 -0500

    Update UserModel

--- src/models/UserModel.ts
+++ src/models/UserModel.ts
@@ -4,8 +4,10 @@
 
 export interface UserModel {
   id: number;
+  account_id: number;
   is_identified: boolean;
   first_name?: string;
   middle_name?: string;
   last_name?: string;
-};
+  last_active_at: string;
+}
