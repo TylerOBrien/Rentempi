17e617eeb0048602c2677d6cd147c384e9a3dff3
Tyler O'Brien <hello@tylerobrien.com>
2021-02-16 18:18:08 -0500

    Make value type a generic

--- src/util/Form.ts
+++ src/util/Form.ts
@@ -12,14 +12,14 @@ export type FormFieldValue = string | number | boolean;
 export type FormLabelType = 'contain' | 'outside';
 export type FormLabelPosition = 'before' | 'after';
 
-export interface FormProps {
+export interface FormProps<T=FormFieldValue> {
   label?: string;
   labelType?: FormLabelType;
   labelPosition?: FormLabelPosition;
   placeholder?: string;
   placeholderTextColor?: ColorValue;
-  value?: FormFieldValue;
-  initialValue?: FormFieldValue;
+  value?: T;
+  initialValue?: T;
   formik?: boolean;
-  onChangeValue?: (value:FormFieldValue) => void;
+  onChangeValue?: (value:T) => void;
 }
