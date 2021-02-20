fa314f02359666082773b8ac52b48642e3d89703
Tyler O'Brien <hello@tylerobrien.com>
2021-02-20 18:39:11 -0500

    Update ScreenDriver

--- src/system/Screen.ts
+++ src/system/Screen.ts
@@ -5,15 +5,43 @@
 import { Dimensions } from 'react-native';
 
 /**
+ * Types/Interfaces
+*/
+
+export interface ScreenDimensions {
+  landscape: {
+    width: number;
+    height: number;
+  },
+  portrait: {
+    width: number;
+    height: number;
+  }
+}
+
+/**
  * Functions
 */
 
 /**
- * @return {Array<number>}
+ * @return {ScreenDimensions}
  */
-function dimensions():Array<number> {
-  const window = Dimensions.get('window');
-  return [ window.width, window.height ];
+function dimensions():ScreenDimensions {
+  const { width, height } = Dimensions.get('window');
+
+  const min = Math.min(width, height);
+  const max = Math.max(width, height);
+
+  return {
+    landscape: {
+      width: max,
+      height: min
+    },
+    portrait: {
+      width: min,
+      height: max
+    }
+  };
 }
 
 /**
