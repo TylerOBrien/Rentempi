0594536e6a6a0f384fd7cb8724ab40d5f36d9c25
Tyler O'Brien <hello@tylerobrien.com>
2021-02-21 00:03:37 -0500

    Add go back button

--- src/forms/guest/ForgotPasswordForm.tsx
+++ src/forms/guest/ForgotPasswordForm.tsx
@@ -4,12 +4,15 @@
 
 import React, { useContext } from 'react';
 import { Formik, FormikProps } from 'formik';
+import { useNavigation } from '@react-navigation/native';
 
 /**
  * Local Imports
 */
 
+import { Button } from '~/components/Button';
 import { Form, Input, ItemPicker, Submit } from '~/components/Form';
+import { Col, Row } from '~/components/Grid';
 import { FormikSubmit } from '~/util/Formik';
 
 /**
@@ -59,6 +62,10 @@ const identityTypes = [
 */
 
 export function ForgotPasswordForm(props:ForgotPasswordFormProps) {
+  /** Hooks **/
+
+  const navigation = useNavigation();
+
   /** Contexts **/
 
   const context = useContext(props.context);
@@ -86,13 +93,27 @@ export function ForgotPasswordForm(props:ForgotPasswordFormProps) {
           field: 'input'
         }}
       />
-      <Submit
-        label='Recover Account'
-        tailwind={{
-          container: 'self-center btn mt-8 rounded-full bg-purple-800',
-          label: 'btn-text text-white'
-        }}
-      />
+      <Row>
+        <Col tailwind='w-1/2'>
+          <Button
+            label='Back'
+            onPress={ () => navigation.goBack() }
+            tailwind={{
+              container: 'self-center btn mt-8 rounded-full bg-gray-500',
+              label: 'btn-text text-white'
+            }}
+          />
+        </Col>
+        <Col tailwind='w-1/2'>
+          <Submit
+            label='Recover Account'
+            tailwind={{
+              container: 'self-center btn mt-8 rounded-full bg-purple-800',
+              label: 'btn-text text-white'
+            }}
+          />
+        </Col>
+      </Row>
     </Form>
   );
   
