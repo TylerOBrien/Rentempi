d46bee6b576899c4c3df364acdaf0515893281d3
Tyler O'Brien <hello@tylerobrien.com>
2021-02-24 22:28:45 -0500

    Add placeholder CacheConfig module

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,6 +4,7 @@
 
 export { ApiConfig } from './Api';
 export { AppConfig } from './App';
+export { CacheConfig } from './Cache';
 export { DateTimeConfig } from './DateTime';
 export { ScreenConfig } from './Screen';
 export { StorageConfig } from './Storage';
